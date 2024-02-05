2a2a4e074a5599f6dcfbecd9e875ae6b7ce05b79
Artem <sevruk04@bk.ru>
2024-02-05 23:27:43 +0300

    Обучение

---
+++ src/components/Body.jsx
@@ -0,0 +1,30 @@
+// function Avatar({ person, size  , id = 1}) {
+//   return (
+//       <div>
+//         <p>{person.name}</p>
+//         <p>{size}</p>
+//         <p>{id}</p>
+//       </div>
+//   );
+// }
+
+// export  const Profile = () => {
+//   return (
+//       <div>
+//           <Avatar
+//               size={100}
+//               person={{
+//                   name: 'Katsuko Saruhashi',
+//                   imageId: 'YfeOqp2',
+//               }}
+//           />
+//           <Avatar
+//               size={80}
+//               person={{
+//                   name: 'Aklilu Lemma',
+//                   imageId: 'OKS67lh',
+//               }}
+//           />
+//       </div>
+//   );
+// }
---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+// const names = [
+//   {
+//     name: "Artem"
+//   },
+//   {
+//     name: "Lera"
+//   },
+//   {
+//     name: "Polina"
+//   }
+// ]
+
+// export const Header = () => {
+//   return (
+//     <div>
+//     <h1>Hello Artem</h1>
+//     {
+//       names.map((name) => {
+//         return (
+//           <div>
+//             <p>{name.name}</p>
+//           </div>
+//         )
+//       })
+
+//       }  
+//     </div>
+//   )
+// }
