45acb319815408cf71657ccd5d5326b9a0ca5551
Artem <sevruk04@bk.ru>
2024-05-20 19:01:27 +0300

    Сделал сумму в корзине и сделал сортировку по цене

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,12 +1,10 @@
-import { getData  , addBasket } from "../api";
+import { getData, addBasket } from "../api";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
 
 const ElementCatalog = ({ product }) => {
-
-  const addBasketProducts = () => addBasket(product)
-
+  const addBasketProducts = () => addBasket(product);
 
   return (
     <div className="catalog">
@@ -32,7 +30,6 @@ const ElementCatalog = ({ product }) => {
       </p>
       <p>{product.price} руб</p>
 
-
       <button onClick={addBasketProducts} className="bay2">
         Добавить в корзину
       </button>
@@ -42,13 +39,49 @@ const ElementCatalog = ({ product }) => {
 
 export const Catalog = () => {
   const params = useParams();
-  console.log(params.categoryId)
-  const query = useQuery(["product", params.categoryId], () => getData(params.categoryId));
+  const query = useQuery(["product", params.categoryId], () =>
+    getData(params.categoryId)
+  );
+  const [sortedProducts, setSortedProducts] = useState([]);
+
+  useEffect(() => {
+    if (query.data) {
+      const productsCopy = [...query.data];
+      const sortedByPriceDesc = productsCopy.sort((a, b) => b.price - a.price);
+      setSortedProducts(sortedByPriceDesc);
+    }
+  }, [query.data]);
+
+  const handleSortByAscending = () => {
+    if (query.data) {
+      const sortedByPriceAsc = [...query.data].sort((a, b) => a.price - b.price);
+      setSortedProducts(sortedByPriceAsc);
+    }
+  };
+
+  const handleSortByDescending = () => {
+    if (query.data) {
+      const sortedByPriceDesc = [...query.data].sort((a, b) => b.price - a.price);
+      setSortedProducts(sortedByPriceDesc);
+    }
+  };
+
   return (
+    <div>
+  <div className="sortPrise"> 
+  <button onClick={handleSortByAscending}>
+        от меньшей к большей
+      </button>
+      <button onClick={handleSortByDescending}>
+        от большей к меньшей
+      </button>
+      </div>
     <div className="catalogContainer">
-      {(query.data || []).map((product) => (
+     
+      {(sortedProducts || []).map((product) => (
         <ElementCatalog product={product} />
       ))}
+    </div>
     </div>
   );
 };
